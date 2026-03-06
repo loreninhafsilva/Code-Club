eeecee20e4fc091acdff1d48a56a4815f3a244ef
loreninhafsilva <lorenasilva.dev@gmail.com>
2026-03-05 22:11:23 -0300

    Erro identificado e alterado

--- 23-02-2026.py
+++ 23-02-2026.py
@@ -10,7 +10,7 @@ levantamento = [
     "Scaptotrigona postica"
 ]
 # 1)
-print("========Ninhos observados========")9
+print("========Ninhos observados========")
 for i, ninho in enumerate(levantamento, start=1): #Usar a função enumerate() para iterar sobre a lista levantamento
     print(f"{i}. {ninho}")
 
