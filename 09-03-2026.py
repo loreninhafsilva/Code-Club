30708446f78185f80e46d4b069747a1fa584eca6
loreninhafsilva <lorenasilva.dev@gmail.com>
2026-03-09 11:02:55 -0300

    +1 adicionado

--- 09-03-2026.py
+++ 09-03-2026.py
@@ -2,7 +2,7 @@ sequencia = 'ACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACG
 primer = "ATGCGTACGTTAGCTA" 
 
 if primer in sequencia:
-    posicao = sequencia.find(primer)
+    posicao = sequencia.find(primer) + 1  # Adiciona 1 para obter a posição correta (baseada em 1)
     print(f"O primer foi encontrado na posição: {posicao}")
 else:
     print("O primer não foi encontrado.")
